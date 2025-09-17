97c9c62365c51fbb3360bf3b30a4e07143034a54
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-09-17 17:52:53 +0900

    💡 [LTC] Array: 0189-rotate-array
    - Time: 0 ms (100%)
    - Space: 66.7 MB (20.76%)
    - Difficulty: Medium
    - Date: 09-17-2025

--- yuyeol/Medium/0189-rotate-array/0189-rotate-array.js
+++ yuyeol/Medium/0189-rotate-array/0189-rotate-array.js
@@ -7,17 +7,17 @@ var rotate = function(nums, k) {
     if(nums.length < k) {
         k = k % nums.length
     }
-    let newNums = []
-    // k 이후 요소를 newNums에 추가
-    for (i = nums.length - k; i < nums.length; i++) {
-        newNums.push(nums[i])
+    nums.reverse();
+    for(i = 0; i < k / 2; i++) {
+        const K_INDEX = k - 1
+        let temp = nums[i]
+        nums[i] = nums[K_INDEX - i];
+        nums[K_INDEX - i] = temp;
     }
-    // k 이전 요소를 newNums에 추가
-    for (i = 0; i < nums.length - k; i++) {
-        newNums.push(nums[i])
-    }
-    // 재할당을 하면 참조가 바뀌므로 참조를 유지한 상태로 배열 요소만 newNums 것으로 전면 교체
-    for (i = 0; i < nums.length; i++){
-        nums[i] = newNums[i]
+    for(i = 0; i < (nums.length - k) / 2; i++) {
+        const LENGTH_INDEX = nums.length - 1
+        let temp = nums[i + k]
+        nums[i + k] = nums[LENGTH_INDEX - i]
+        nums[LENGTH_INDEX- i] = temp
     }
 };
