4b1a781a1c7217e3febf0899f7c3fd5bd1b79f13
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-09-17 17:53:40 +0900

    💡 [LTC] Array: 0189-rotate-array
    - Time: 1 ms (80.66%)
    - Space: 65.8 MB (41.58%)
    - Difficulty: Medium
    - Date: 09-17-2025

--- yuyeol/Medium/0189-rotate-array/0189-rotate-array.js
+++ yuyeol/Medium/0189-rotate-array/0189-rotate-array.js
@@ -7,6 +7,10 @@ var rotate = function(nums, k) {
     if(nums.length < k) {
         k = k % nums.length
     }
+    // in-place 방식 사용
+    // 1. 배열 전체 reverse
+    // 2. k에 해당하는 앞부분 길이의 절반 순회를 통한 reverse
+    // 3. 나머지에 해당하는 뒷부분 길이의 절반 순회를 통한 reverse
     nums.reverse();
     for(i = 0; i < k / 2; i++) {
         const K_INDEX = k - 1
@@ -18,6 +22,6 @@ var rotate = function(nums, k) {
         const LENGTH_INDEX = nums.length - 1
         let temp = nums[i + k]
         nums[i + k] = nums[LENGTH_INDEX - i]
-        nums[LENGTH_INDEX- i] = temp
+        nums[LENGTH_INDEX - i] = temp
     }
 };
