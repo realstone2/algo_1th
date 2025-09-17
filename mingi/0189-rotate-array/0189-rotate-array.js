6e5d2b434e600aa2f82f7f8b811a87f662d688ad
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-09-17 23:19:04 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ mingi/0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    for(let i=0;i<k;i++){
+        const lastElement = nums.pop();
+        nums.splice(0, 0, lastElement);
+    }
+    return nums;
+
+};
