e1a14a1d9302e0940615b47767da07dc76f78765
Ga eun Lee <35989487+tooth-is-silver@users.noreply.github.com>
2025-09-18 19:29:39 +0900

    09-18-2025 19 ms (55.61%) 0217-contains-duplicate

--- tooth-is-silver/0217-contains-duplicate/0217-contains-duplicate.js
+++ tooth-is-silver/0217-contains-duplicate/0217-contains-duplicate.js
@@ -6,13 +6,13 @@ var containsDuplicate = function(nums) {
     // 값이 중복으로 들어왔는지 아닌지 체크
     // nums를 반복문으로 돌려서 배열안에 값이 없으면 넣고, 다음으로
     // 배열안에 값이 있으면 바로 true 반환
-    const checked = []
+    // 첫번째 시도 includes로 확인했으나 time limit exceeded 로 set으로 수정 
+    const checkedSet = new Set();
     for (let i = 0; i < nums.length; i++) {
-        if (checked.includes(nums[i])) {
-            console.log(nums[i], '가 잇음', checked)
+        if (checkedSet.has(nums[i])) {
             return true
         } else {
-            checked.push(nums[i])
+            checkedSet.add(nums[i])
         }
     }
     return false
