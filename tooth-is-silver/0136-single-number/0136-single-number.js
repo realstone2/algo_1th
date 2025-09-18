f56eb3e837e795dd00a3b91416546bd46f193d4e
Ga eun Lee <35989487+tooth-is-silver@users.noreply.github.com>
2025-09-19 01:25:20 +0900

    09-19-2025 1 ms (72.22%) 0136-single-number

--- tooth-is-silver/0136-single-number/0136-single-number.js
+++ tooth-is-silver/0136-single-number/0136-single-number.js
@@ -5,9 +5,13 @@
 var singleNumber = function(nums) {
     // nums에서 반복되지 않는 것이 딱 하나 있다
     // findIndex를 사용하면 index가 여러개인지 확인할수 있지않나?
-    const setNums = Array.from(new Set(nums));
-    for (let i = 0; i < setNums.length; i++) {
-        if (nums.filter(num => num === setNums[i]).length === 1)
-        return setNums[i]
+    // 처음에 for문 내부에 filter로 해서 했는데 memory runtime이 너무 오래걸려서 다른 방법으로시도
+
+    // ai한테 비트연산자로 푸는 방법을 제안받았다. nums를 순회하면서 비트연산자 XOR ^=로 계산하면
+    // 2회 나온 숫자는 비트 계산시 0과 1이 계산되서 없어지고 한번만 나온 숫자는 계산이 되지 않아 그대로 남아있음
+    let result = 0;
+    for (let num of nums) {
+        result ^= num
     }
+    return result
 };
