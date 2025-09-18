6bd021effd44cc5fd81290daa82fc26ced0b5b0b
Ga eun Lee <35989487+tooth-is-silver@users.noreply.github.com>
2025-09-19 01:26:31 +0900

    09-19-2025 15 ms (17.44%) 0136-single-number

--- tooth-is-silver/0136-single-number/0136-single-number.js
+++ tooth-is-silver/0136-single-number/0136-single-number.js
@@ -9,9 +9,18 @@ var singleNumber = function(nums) {
 
     // ai한테 비트연산자로 푸는 방법을 제안받았다. nums를 순회하면서 비트연산자 XOR ^=로 계산하면
     // 2회 나온 숫자는 비트 계산시 0과 1이 계산되서 없어지고 한번만 나온 숫자는 계산이 되지 않아 그대로 남아있음
-    let result = 0;
-    for (let num of nums) {
-        result ^= num
+    // let result = 0;
+    // for (let num of nums) {
+    //     result ^= num
+    // }
+    // return result
+
+    // ai도움 없이 다른 방법으로도 시도해봄.
+    // sort 후에 앞뒤로 비교
+    const sortNums = nums.sort();
+    for (let i = 0; i < sortNums.length; i += 2) {
+        if (sortNums[i] !== sortNums[i + 1]) {
+            return sortNums[i]
+        }
     }
-    return result
 };
