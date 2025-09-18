f488efa148bb3d261e776d11c7723f0819dd1594
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-09-18 12:42:44 +0900

    💡 [LTC] Array: 0217-contains-duplicate
    - Time: 23 ms (37.23%)
    - Space: 72.2 MB (86.23%)
    - Difficulty: Easy
    - Date: 09-18-2025

--- yuyeol/Easy/0217-contains-duplicate/0217-contains-duplicate.js
+++ yuyeol/Easy/0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    // set과 array의 길이만 비교하여 중복여부를 확인
+    // set과 array의 길이만 비교하여 중복여부를 확인함
     const numsSet = new Set(nums);
     return numsSet.size !== nums.length;
 };
