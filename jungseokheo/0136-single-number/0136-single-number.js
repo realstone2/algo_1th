c7e54975bfcde6c4b2f496d6b255c5cdad7f22fa
jungseok.heo <99116541+heojungseok@users.noreply.github.com>
2025-09-19 13:14:46 +0900

    💡 [LTC] Array: 0136-single-number
    - Time: 8 ms (40.46%)
    - Space: 59.7 MB (14.75%)
    - Difficulty: Easy
    - Date: 09-19-2025

--- jungseokheo/0136-single-number/0136-single-number.js
+++ jungseokheo/0136-single-number/0136-single-number.js
@@ -8,4 +8,24 @@ const singleNumber = function(nums) {
         set.has(num) ? set.delete(num) : set.add(num);
     }
     return [...set][0];
+    /**
+    모범 답안
+    [4,1,2,1,2]를 모두 XOR하면:
+    4 ^ 1 ^ 2 ^ 1 ^ 2
+
+    교환법칙으로 같은 수끼리 묶으면:
+    = 4 ^ (1 ^ 1) ^ (2 ^ 2)
+
+    같은 수끼리 XOR하면 0:
+    = 4 ^ 0 ^ 0
+    
+    0과 XOR하면 자기 자신: 
+    =4
+    let result = 0;
+    for (let num of nums) {
+        result ^= num;
+    }
+    return result;
+     */
+
 };
