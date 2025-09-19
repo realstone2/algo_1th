c5891c9d7df36d06b952ebddd6162dfc2b3b779f
ChangJun Park <40492343+ckdwns9121@users.noreply.github.com>
2025-09-19 18:04:13 +0900

    💡 [LTC] Array: 0136-single-number
    - Time: 3 ms (49.5%)
    - Space: 58.1 MB (29.61%)
    - Difficulty: Easy
    - Date: 09-19-2025

--- changjun/0136-single-number/0136-single-number.js
+++ changjun/0136-single-number/0136-single-number.js
@@ -2,12 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
-    
-    let result = 0;
 
-    for(let num of nums){
-        result ^= num;
+ // [1,2,2]
+ // [1,2]
+ // [2,2,2,2,2,2,2,1] =1
+// [-1,-1,-1,-1, 11,-2-,2] = 11
+ // output = 1
+
+ // 중복되는 숫자말고 중복 안되는 숫자 찾기
+
+var singleNumber = function(nums) {
+  const newSet = new Set();
+  const newArr = [];
+
+  for(const num of nums){
+    // 이미 있으면
+    if(newSet.has(num)){
+        newSet.delete(num);
     }
-    return result;
+    else{
+    newSet.add(num);
+    }
+  }  
+    return [...newSet][0]
 };
