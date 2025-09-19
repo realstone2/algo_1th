d3ae814c39ad92dd833b3bd00c72d048b26b92a7
jungseok.heo <99116541+heojungseok@users.noreply.github.com>
2025-09-19 13:13:26 +0900

    💡 [LTC] Array: 0136-single-number
    - Time: 1 ms (72.24%)
    - Space: 57.7 MB (35.47%)
    - Difficulty: Easy
    - Date: 09-19-2025

--- jungseokheo/0136-single-number/0136-single-number.js
+++ jungseokheo/0136-single-number/0136-single-number.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
+const singleNumber = function(nums) {
     let set = new Set();
     for (num of nums) {
         set.has(num) ? set.delete(num) : set.add(num);
