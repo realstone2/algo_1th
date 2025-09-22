96df6d8bcc4bc6c5c161b48f75d641de68a98005
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-09-22 13:18:51 +0900

    💡 [LTC] Array: 0350-intersection-of-two-arrays-ii
    - Time: 3 ms (36.67%)
    - Space: 56.7 MB (23.12%)
    - Difficulty: Easy
    - Date: 09-22-2025

--- yuyeol/Easy/0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ yuyeol/Easy/0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -3,41 +3,25 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-
-const createNumsCountObj = (nums) => {
-    const numsCountObj = {}
-    for (let i = 0; i < nums.length; i++) {
-        if(Object.keys(numsCountObj).includes(String(nums[i]))) {
-            numsCountObj[nums[i]] += 1;
-        } else {
-            numsCountObj[nums[i]] = 1;
-        }
-    }
-    return numsCountObj;
-}
-
 var intersect = function(nums1, nums2) {
-    // 원소를 key, 갯수를 value로 가지는 객체로 두 배열 변환
-    const numsCountObj1 = createNumsCountObj(nums1)
-    const numsCountObj2 = createNumsCountObj(nums2)
-    const objKeys1 = Object.keys(numsCountObj1);
-    const objKeys2 = Object.keys(numsCountObj2);
-
-    // 두 객체가 공통적으로 가지고있는 아이템 배열에 저장
-    const commonItems = []
-    for(let i = 0; i < objKeys1.length; i++) {
-        if(numsCountObj2[objKeys1[i]]) {
-            commonItems.push(Number(objKeys1[i]))
+    // 형변환의 번거로움 배제를 위해 맵 자료형으로 개선
+    const numsMap1 = new Map();
+    for(let i = 0; i < nums1.length; i++) {
+        // 맵에 nums1 배열에 대한 key: 요소 값, value: 갯수 저장
+        if(numsMap1.has(nums1[i])) {
+            numsMap1.set(nums1[i], numsMap1.get(nums1[i]) + 1)
+        } else {
+            numsMap1.set(nums1[i], 1)
         }
     }
-
-    // 저장된 공통 아이템을 갯수만큼 배열에 추가
     const result = []
-    for(let i = 0; i < commonItems.length; i++) {
-        const commonItem = commonItems[i];
-        for(j = 0; j < Math.min(numsCountObj1[commonItem], numsCountObj2[commonItem]); j++) {
-            result.push(commonItems[i])
+    for(let i = 0; i < nums2.length; i++) {
+        // nums2의 현재 숫자가 numsMap1에 있는지 확인 && 갯수가 0개면 실행 생략
+        // 있으면 결과에 추가하고 카운트를 1 감소
+        if(numsMap1.has(nums2[i]) && numsMap1.get(nums2[i]) > 0) {
+            numsMap1.set(nums2[i], numsMap1.get(nums2[i]) - 1)
+            result.push(nums2[i])
         }
-    }
+    }   
     return result;
 };
