303541a2b203c1fc3812e115ed71fc084f14680a
jungseok.heo <99116541+heojungseok@users.noreply.github.com>
2025-09-23 12:34:48 +0900

    💡 [LTC] Array: 0066-plus-one
    - Time: 0 ms (100%)
    - Space: 54.3 MB (19.16%)
    - Difficulty: Easy
    - Date: 09-23-2025

---
+++ jungseokheo/0066-plus-one/0066-plus-one.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+const plusOne = function(digits) {
+    let plus = 1;
+    for (i = digits.length - 1; i >= 0; i--) {
+       let sum = digits[i] + plus;
+       
+       sum % 10 > 0 ? digits[i] = sum : digits[i] = sum % 10;
+       plus = Math.floor(sum / 10);
+    }
+
+    if (plus === 1) {
+        digits.unshift(plus);
+    }
+
+    return digits;
+};
