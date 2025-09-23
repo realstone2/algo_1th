c4c47cfb2e25f919269989dd6d272d6d7d23e16f
진돌 <79455437+realstone2@users.noreply.github.com>
2025-09-23 23:41:56 +0900

    💡 [LTC] Array: 0066-plus-one
    - Time: 0 ms (100%)
    - Space: 54.4 MB (19.16%)
    - Difficulty: Easy
    - Date: 09-23-2025

---
+++ jinseok/0066-plus-one/0066-plus-one.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+
+    const result = []
+    let i = digits.length - 1;
+    let up = false
+    while(i >= 0){
+        const newNumber = digits[i] + 1
+        if(newNumber < 10){
+            digits.splice(i, 1, newNumber)
+            up = false
+            break;
+        } else {
+            up = true
+            digits.splice(i, 1, 0)
+        }
+
+        i--
+    }
+
+    if(up){
+        return [1, ...digits];
+    }
+    return digits
+};
