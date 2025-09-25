ea90d73877e2c34af06fa4b7ee1da9a9de127519
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-09-25 11:35:20 +0900

    💡 [LTC] Array: 0001-two-sum
    - Time: 1 ms (87.98%)
    - Space: 56.7 MB (19.57%)
    - Difficulty: Easy
    - Date: 09-25-2025

--- yuyeol/Easy/0001-two-sum/0001-two-sum.js
+++ yuyeol/Easy/0001-two-sum/0001-two-sum.js
@@ -4,6 +4,7 @@
  * @return {number[]}
  */
 
+/** 
 // 브루트포스 방식으로 접근
 var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++) {
@@ -12,5 +13,20 @@ var twoSum = function(nums, target) {
             // 인덱스가 다를 경우 더해보고 타겟과 값이 동일하면 배열로 리턴
             if(nums[i] + nums[j] === target) return [i, j];
         }
+    }
+};
+*/
+
+
+// 해시 테이블 방식으로 접근
+var twoSum = function(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        // 해당 인덱스의 원소값이 필요로 하는 값
+        let needed = target - nums[i];
+        // 필요로 하는 값을 찾았으면 인덱스를 묶어서 리턴
+        if(map.has(needed)) return [i, map.get(needed)];
+        // 못찾았으면 원소값을 key, 인덱스를 value로 저장하여 순회한 원소들을 기억해둠
+        map.set(nums[i], i);
     }
 };
