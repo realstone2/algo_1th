ad972b23c7f006b741b7d74b975a1adc3a5dc0a5
Ga eun Lee <35989487+tooth-is-silver@users.noreply.github.com>
2025-09-26 00:08:27 +0900

    09-26-2025 29 ms (43.93%) 0001-two-sum

--- tooth-is-silver/0001-two-sum/0001-two-sum.js
+++ tooth-is-silver/0001-two-sum/0001-two-sum.js
@@ -10,17 +10,14 @@ var twoSum = function(nums, target) {
     // 핵심)
     // 변수 1개를 둔다. 반복문을 돌린다. 대신 변수(index) 이후의 값으로 하나씩 더한 값을 target과 비교한다
     // 두 값을 더한 값이 target이 되면 return;
-    let firstIdx = 0;
-    let secondIdx = 1;
-    while (firstIdx < nums.length) {
-        if (nums[firstIdx] + nums[secondIdx] === target) {
-            return [firstIdx, secondIdx]
-        } else {
-            secondIdx++
-        }
-        if (secondIdx === nums.length) {
-            firstIdx++
-            secondIdx = firstIdx + 1
+    for (let i = 0; i < nums.length; i++) {
+        // console.log('i는', i)
+        const firstNum = nums[i]
+        for (let j = i + 1; j < nums.length; j++){
+            const secondNum = nums[j]
+            if (firstNum + secondNum === target) {
+                return [i, j]
+            }
         }
     }
 };
