27bab0cd94907b680d3c6e3aac5730071dc75401
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-09-25 11:04:12 +0900

    💡 [LTC] Array: 0001-two-sum
    - Time: 29 ms (43.93%)
    - Space: 54.3 MB (79.77%)
    - Difficulty: Easy
    - Date: 09-25-2025

--- yuyeol/Easy/0001-two-sum/0001-two-sum.js
+++ yuyeol/Easy/0001-two-sum/0001-two-sum.js
@@ -7,9 +7,8 @@
 // 브루트포스 방식으로 접근
 var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++) {
-        for(let j = 0; j < nums.length; j++) {
-            // 같은 인덱스끼리는 더할 수 없으므로 무시
-            if(i === j) continue;
+        // j가 i + 1이 되면 j가 i 이전 인덱스를 순회할 필요가 없어져서 이미 계산해본 것을 중복 계산할 필요가 없어짐
+        for(let j = i + 1; j < nums.length; j++) {
             // 인덱스가 다를 경우 더해보고 타겟과 값이 동일하면 배열로 리턴
             if(nums[i] + nums[j] === target) return [i, j];
         }
