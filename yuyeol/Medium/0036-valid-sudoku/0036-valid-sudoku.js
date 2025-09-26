29c6b9f31f7927a45ee51b6b252a5df84ccf38fd
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-09-26 12:33:16 +0900

    💡 [LTC] Array: 0036-valid-sudoku
    - Time: 6 ms (47.14%)
    - Space: 55.8 MB (92.59%)
    - Difficulty: Medium
    - Date: 09-26-2025

--- yuyeol/Medium/0036-valid-sudoku/0036-valid-sudoku.js
+++ yuyeol/Medium/0036-valid-sudoku/0036-valid-sudoku.js
@@ -19,9 +19,15 @@ var isValidSudoku = function(board) {
             colSet.add(board[j][i]);
         }
     }
+    // 총 9개의 3*3 박스에 중복 없는지 확인
+    /* 
+    row: 몫 * 3 col: 나머지 * 3
+    * 3을 하는 이유? box가 3*3이라서 순회 간격이 3칸이기 때문. 그래서 박스 단위로 건너뛰려면 *3이 필요.
+    12*12에 4*4 박스를 순회하는거였다면 나머지와 몫을 3으로 구하고, 건너뛰기 위해 *4를 했을 것임.
+    **/
     for(let i = 0; i < 9; i++) {
-        const startRow = Math.floor(i / 3) * 3; // 몫 * 3
-        const startCol = i % 3 * 3; // 나머지 * 3
+        const startRow = Math.floor(i / 3) * 3; 
+        const startCol = i % 3 * 3; 
         const boxSet = new Set();
         for(let j = 0; j < 3; j++) {
             for (let k = 0; k < 3; k++) {
