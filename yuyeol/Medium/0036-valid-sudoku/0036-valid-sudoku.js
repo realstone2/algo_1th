4346d9a4eb03822f3eec335207e2beb763e59318
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-09-26 12:26:17 +0900

    💡 [LTC] Array: 0036-valid-sudoku
    - Time: 7 ms (34.8%)
    - Space: 58 MB (34.29%)
    - Difficulty: Medium
    - Date: 09-26-2025

--- yuyeol/Medium/0036-valid-sudoku/0036-valid-sudoku.js
+++ yuyeol/Medium/0036-valid-sudoku/0036-valid-sudoku.js
@@ -3,6 +3,7 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
+    // 행에 중복 없는지 확인
     for(let i = 0; i < board.length; i++) {
         const rowSet = new Set();
         for(let j = 0; j < board.length; j++){
@@ -10,6 +11,7 @@ var isValidSudoku = function(board) {
             rowSet.add(board[i][j]);
         }
     }
+    // 열에 중복 없는지 확인
     for(let i = 0; i < board.length; i++) {
         const colSet = new Set();
         for(let j = 0; j < board.length; j++){
@@ -19,12 +21,16 @@ var isValidSudoku = function(board) {
     }
     for(let i = 0; i < 9; i++) {
         const startRow = Math.floor(i / 3) * 3; // 몫 * 3
-        const startCol = i % 3 * 3;
+        const startCol = i % 3 * 3; // 나머지 * 3
         const boxSet = new Set();
         for(let j = 0; j < 3; j++) {
             for (let k = 0; k < 3; k++) {
-                if(boxSet.has(board[startRow + j][startCol + k]) && board[startRow + j][startCol + k] !== ".") return false;
-                boxSet.add(board[startRow + j][startCol + k]);
+                const row = startRow + j
+                const col = startCol + k
+                if(boxSet.has(board[row][col]) && board[row][col] !== ".") {
+                    return false;
+                } 
+                boxSet.add(board[row][col]);
             }
         }
     }
