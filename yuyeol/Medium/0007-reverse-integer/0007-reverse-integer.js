f819cd576f5fac560fc31df8d5c5626cd3650f84
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-10-01 19:36:21 +0900

    💡 [LTC] Math: 0007-reverse-integer
    - Time: 63 ms (11.64%)
    - Space: 57.2 MB (7.19%)
    - Difficulty: Medium
    - Date: 10-01-2025

--- yuyeol/Medium/0007-reverse-integer/0007-reverse-integer.js
+++ yuyeol/Medium/0007-reverse-integer/0007-reverse-integer.js
@@ -7,14 +7,17 @@ const MAX = Math.pow(2, 31) - 1;
 const MIN = -Math.pow(2, 31);
 
 var reverse = function(x) {
-    const isMinus = x < 0;
+    const isNegative = x < 0;
     const xString = String(Math.abs(x));
-    let res = 0;
+    let sum = 0;
     for(i = 0; i < xString.length; i++) {
         const digit = Number(xString[i]);
         const reversedDigit = digit * Math.pow(10, i);
-        res += reversedDigit;
-        if(res > MAX) return 0; 
+        sum += reversedDigit;
+        if(sum > MAX) return 0; 
     }
-    return isMinus ? -res : res
+
+    const res = isNegative ? -sum : sum;
+    if(res < MIN || res > MAX) return 0;
+    return res;
 };
