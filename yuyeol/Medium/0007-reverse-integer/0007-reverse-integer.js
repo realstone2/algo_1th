1b3254242b14d1d53230abac82b891c239e43abc
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-10-01 19:33:14 +0900

    💡 [LTC] Math: 0007-reverse-integer
    - Time: 64 ms (9.79%)
    - Space: 55.8 MB (37.72%)
    - Difficulty: Medium
    - Date: 10-01-2025

---
+++ yuyeol/Medium/0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+const MAX = Math.pow(2, 31) - 1;
+const MIN = -Math.pow(2, 31);
+
+var reverse = function(x) {
+    const isMinus = x < 0;
+    const xString = String(Math.abs(x));
+    let res = 0;
+    for(i = 0; i < xString.length; i++) {
+        const digit = Number(xString[i]);
+        const reversedDigit = digit * Math.pow(10, i);
+        res += reversedDigit;
+    }
+    if(res > MAX) return 0;
+    return isMinus ? -res : res
+};
