0a6c060394452f3f5f3fdf069c9049036344f5f5
ChaeYoungJin <124546770+devchaeyoung@users.noreply.github.com>
2025-10-02 19:28:15 +0900

    💡 [LTC] Hash Table: 0387-first-unique-character-in-a-string
    - Time: 32 ms (59.71%)
    - Space: 60.8 MB (26.97%)
    - Difficulty: Easy
    - Date: 10-02-2025

---
+++ ChaeYoungJin/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const charCount = new Map()
+    for (let char of s) {
+        charCount.set(char, (charCount.get(char) || 0) + 1)
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        if (charCount.get(s[i]) === 1) return i
+    }
+    
+    return -1
+    
+};
