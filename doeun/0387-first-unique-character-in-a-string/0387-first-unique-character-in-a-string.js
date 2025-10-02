48b11dc9966d1d59dd42ed6f0a548978be88ff19
Doeun <112849712+nemobim@users.noreply.github.com>
2025-10-02 09:45:23 +0900

    💡 [LTC] Hash Table: 0387-first-unique-character-in-a-string
    - Time: 25 ms (83.52%)
    - Space: 57.7 MB (93.86%)
    - Difficulty: Easy
    - Date: 10-02-2025

--- doeun/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ doeun/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -4,33 +4,33 @@
  */
 
 //20~24초
-// const firstUniqChar = function (s) {
-//     // 중복된 문자가 아니라면 앞 뒤로 수색했을때 인덱스가 같다.
-//     for (let i = 0; i < s.length; i++) {
-//         if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
-//             return i;
-//         }
-//     }
-//     return -1;
-// }; 
-
-
-
 const firstUniqChar = function (s) {
-    // Map에 갯수 담고 1개인거 반환하기 하면 1반복으로 끝낼 수 있
-    const count = new Map();
-
-    // 각 문자 횟수 세기
-    for (let char of s) {
-        count.set(char, (count.get(char) || 0) + 1);
-    }
-
-    // 등장 횟수 1 인거 찾아서 반환
+    // 중복된 문자가 아니라면 앞 뒤로 수색했을때 인덱스가 같다.
     for (let i = 0; i < s.length; i++) {
-        if (count.get(s[i]) === 1) {
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
             return i;
         }
     }
-
     return -1;
-};
+}; 
+
+
+
+// const firstUniqChar = function (s) {
+//     // Map에 갯수 담고 1개인거 반환하기 하면 1반복으로 끝낼 수 있
+//     const count = new Map();
+
+//     // 각 문자 횟수 세기
+//     for (let char of s) {
+//         count.set(char, (count.get(char) || 0) + 1);
+//     }
+
+//     // 등장 횟수 1 인거 찾아서 반환
+//     for (let i = 0; i < s.length; i++) {
+//         if (count.get(s[i]) === 1) {
+//             return i;
+//         }
+//     }
+
+//     return -1;
+// };
