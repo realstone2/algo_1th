d66e40f3e32772e7c50851776e51f583c840046d
Doeun <112849712+nemobim@users.noreply.github.com>
2025-10-02 09:39:03 +0900

    💡 [LTC] Hash Table: 0387-first-unique-character-in-a-string
    - Time: 25 ms (83.52%)
    - Space: 56.6 MB (99.4%)
    - Difficulty: Easy
    - Date: 10-02-2025

--- doeun/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ doeun/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -4,11 +4,25 @@
  */
 const firstUniqChar = function (s) {
     // 중복이 아니다.. 앞 뒤로 수색했을때 인덱스가 같다.
-    const repeats = [...s];
-    for (let i = 0; i < repeats.length; i++) {
-        if (repeats.indexOf(repeats[i]) === repeats.lastIndexOf(repeats[i])) {
+    // 반복을 너무 돌았나 끝자락에 있음
+    for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
             return i;
         }
     }
     return -1;
 };
+
+// const firstUniqChar = function (s) {
+//     // Map에 갯수 담고 1개인거 반환하기 하면 1반복으로 끝낼 수 있
+//     const count = new Map();
+
+//     // 각 문자 횟수 세기
+//   for (let i = 0; i < s.length; i++) {
+//         if (count.get(s[i]) === 1) {
+//             return i;
+//         }
+//     }
+    
+//     return -1;
+// };
