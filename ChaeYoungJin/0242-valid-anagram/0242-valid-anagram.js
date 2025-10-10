6b721547ae8dbc461dd167684c4dc1fa9d92d55f
ChaeYoungJin <124546770+devchaeyoung@users.noreply.github.com>
2025-10-10 13:58:10 +0900

    💡 [LTC] Hash Table: 0242-valid-anagram
    - Time: 15 ms (54.58%)
    - Space: 60.7 MB (15.62%)
    - Difficulty: Easy
    - Date: 10-10-2025

--- ChaeYoungJin/0242-valid-anagram/0242-valid-anagram.js
+++ ChaeYoungJin/0242-valid-anagram/0242-valid-anagram.js
@@ -4,7 +4,20 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-     s = s.split('').sort().join('')
-     t = t.split('').sort().join('')
-    return (s === t)
+    if(s.length !== t.length) return false
+    
+    const charCount = {}
+    
+    for (let char of s) {
+        charCount[char] = (charCount[char] || 0) + 1
+    }
+    
+    for (let char of t) {
+        if (!charCount[char]) {
+            return false
+        }
+        charCount[char]--
+    }
+    
+    return true
 };
