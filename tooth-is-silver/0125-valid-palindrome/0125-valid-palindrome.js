8848c4d62b439754afb0dd8aba884bdcf0369910
Ga eun Lee <35989487+tooth-is-silver@users.noreply.github.com>
2025-10-14 01:03:02 +0900

    8 ms (29.11%) 0125-valid-palindrome

--- tooth-is-silver/0125-valid-palindrome/0125-valid-palindrome.js
+++ tooth-is-silver/0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,12 +4,16 @@
  */
 var isPalindrome = function(s) {
     const asciiRegex = /^[a-zA-Z0-9]+$/
-    s = [...s.toLowerCase()].filter(word => asciiRegex.test(word)).join('')
+    let str = ''
+    // s = [...s.toLowerCase()].filter(word => asciiRegex.test(word)).join('')
+    for (const word of s.toLowerCase()) {
+        if (asciiRegex.test(word)) str += word
+    }
     
     let first = 0;
-    let second = s.length - 1;
+    let second = str.length - 1;
     while (first < second) {
-        if (s[first] !== s[second]) return false;
+        if (str[first] !== str[second]) return false;
         first++
         second--
     }
