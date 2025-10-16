43d427a1476e937ef2160bd6b7e7d445d9cd35b2
진돌 <79455437+realstone2@users.noreply.github.com>
2025-10-16 23:08:37 +0900

    💡 [LTC] Linked List: 0237-delete-node-in-a-linked-list
    - Time: 66 ms (5.31%)
    - Space: 56.5 MB (77.32%)
    - Difficulty: Medium
    - Date: 10-16-2025

--- jinseok/0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.js
+++ jinseok/0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.js
@@ -10,7 +10,7 @@
  * @return {void} Do not return anything, modify node in-place instead.
  */
 var deleteNode = function(node) {
-    node.val = node.next.val
-    node.next = node.next.next
+    node.val=node.next.val;
+    node.next=node.next.next;
     
 };
