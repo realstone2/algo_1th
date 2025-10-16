1161866492aec0763759f5654e822deed05d6eef
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-10-16 15:41:14 +0900

    💡 [LTC] Array: 0014-longest-common-prefix
    - Time: 3 ms (31.84%)
    - Space: 54.7 MB (52.3%)
    - Difficulty: Easy
    - Date: 10-16-2025

--- yuyeol/Easy/0014-longest-common-prefix/0014-longest-common-prefix.js
+++ yuyeol/Easy/0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -4,12 +4,16 @@
  */
 var longestCommonPrefix = function(strs) {
     const firstItem = strs[0]
+    // 공통 str을 가지고 있는 마지막 인덱스
     let finalIndex = firstItem.length;
     for(let i = 1; i < strs.length; i++){
         for( let j = 0; j < finalIndex; j++) {
+            // 0번째 인덱스의 문자열과 이외 문자열의 공통 str 비교
             if(strs[i][j] !== strs[0][j]) {
-                if(j === 0) return ""
-                finalIndex = j
+                // 공통이 없으면 즉시 빈문자열 리턴
+                if(j === 0) return "";
+                // 마지막 인덱스 갱신
+                finalIndex = j;
             }
         }
 
