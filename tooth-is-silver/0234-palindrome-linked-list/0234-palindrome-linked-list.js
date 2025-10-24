450b52ba653082a29330e2c5005409e1fac5c5c9
Ga eun Lee <35989487+tooth-is-silver@users.noreply.github.com>
2025-10-24 23:18:46 +0900

    16 ms (29.52%) 0234-palindrome-linked-list

---
+++ tooth-is-silver/0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    const arr = [];
+    let current = head;
+
+    while (current !== null) {
+        arr.push(current.val);
+        current = current.next;
+    }
+
+    let left = 0;
+    let right = arr.length - 1;
+
+    // 양쪽에서 한칸씩 이동하면서 같은지 체크
+    while (left < right) {
+        if (arr[left] !== arr[right]) return false;
+        left++;
+        right--;
+    }
+
+    return true;
+};
