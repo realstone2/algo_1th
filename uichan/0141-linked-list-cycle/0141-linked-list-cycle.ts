a75e0a784bb0dbc76b9168acde9733e5ea70eb1c
이의찬 <cksxkr5193@naver.com>
2025-10-27 01:49:58 +0900

    💡 [LTC] Hash Table: 0141-linked-list-cycle
    - Time: 53 ms (33.04%)
    - Space: 58 MB (65.98%)
    - Difficulty: Easy
    - Date: 10-27-2025

---
+++ uichan/0141-linked-list-cycle/0141-linked-list-cycle.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    if (head === null || head.next === null) {
+        return false;
+    }
+
+    let slow: ListNode | null = head;
+    let fast: ListNode | null = head.next;
+
+    while (slow !== fast) {
+        if (fast === null || fast.next === null) {
+            return false;
+        }
+        slow = slow!.next;
+        fast = fast.next.next;
+    }
+
+    return true;
+}
