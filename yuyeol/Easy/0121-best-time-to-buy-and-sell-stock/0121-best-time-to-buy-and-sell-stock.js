c75e2b54f38425c6d1404b628d0cb837a8598761
정유열 <52055504+Yuyeol@users.noreply.github.com>
2025-11-06 15:36:12 +0900

    💡 [LTC] Array: 0121-best-time-to-buy-and-sell-stock
    - Time: 1 ms (96.03%)
    - Space: 65.9 MB (35.49%)
    - Difficulty: Easy
    - Date: 11-06-2025

--- yuyeol/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ yuyeol/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -6,12 +6,13 @@ var maxProfit = function(prices) {
     let minPrice = prices[0];
     let profit = 0;
     for (let i = 1; i < prices.length; i++) {
-        if(prices[i - 1] < prices[i]) {
-            profit = profit < prices[i] - minPrice ? prices[i] - minPrice : profit;
+        //
+        if(prices[i - 1] < prices[i] && profit < prices[i] - minPrice) {
+            profit = prices[i] - minPrice;
         } 
-        if(prices[i] < minPrice) {
-            minPrice = prices[i];
-        }
+        // 최솟값을 수집
+        if(prices[i] < minPrice) minPrice = prices[i];
+        
     }
     return profit;
 };
