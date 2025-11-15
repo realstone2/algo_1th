29f418e60c3ef5f23ea9baf62be3c6cd97206c84
Ga eun Lee <35989487+tooth-is-silver@users.noreply.github.com>
2025-11-16 02:40:28 +0900

    1523 ms (5.46%) 0204-count-primes

---
+++ tooth-is-silver/0204-count-primes/0204-count-primes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    if (n <= 2) return 0;
+
+    const isPrime = Array.from({length: n}, (v, i) => i < 2 ? false : true )
+
+    // 앞에서 이미 소수로 계산된 숫자들은 제외하고 계산
+    const limit = Math.sqrt(n);
+
+    for (let i = 2; i < limit + 1; i++) {
+        for (let j = i * i; j < n; j += i) {
+            isPrime[j] = false;
+        }
+    }
+    
+    return isPrime.filter(item => item === true).length;
+};
