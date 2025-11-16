e9fb2be4a67f340b190d2fe8b9daa651a0674992
Dahm <60846068+j2h30728@users.noreply.github.com>
2025-11-16 20:56:45 +0900

    💡 [LTC] Array: 0204-count-primes
    - Time: 256 ms (40.51%) 
    - Spec: 120 MB (24.46%) 
    - Defficulty: Medium 
    - Date: 11-16-2025

---
+++ jihyeon/LeetCode/Medium/0204-count-primes/0204-count-primes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    if(n < 2) return 0;
+
+    const isPrime = new Array(n).fill(true);
+    isPrime[0] = isPrime[1] = false;
+
+    for(let i = 2; i * i < n; i++){
+        if(!isPrime[i]) continue;
+
+        for(let j = i * i; j < n; j += i){
+            isPrime[j] = false;
+        }
+    }
+    return isPrime.filter(Boolean).length;
+}
