0f8917f4c1ddad0ccb160328edf2167d815a6cb3
박준형 <33216504+BBAK-jun@users.noreply.github.com>
2025-11-16 21:07:01 +0900

    Time: 12 ms (96.9%), Space: 70.7 MB (68.22%) - LeetHub

---
+++ bbak-jun/0384-shuffle-an-array/0384-shuffle-an-array.js
@@ -0,0 +1,31 @@
+var Solution = function(nums) {
+    this.origin = [...nums];
+    this.value = [...nums];
+};
+
+/**
+ * @return {number[]}
+ */
+Solution.prototype.reset = function() {
+    this.value = [...this.origin];
+    return this.value
+};
+
+/**
+ * @return {number[]}
+ */
+Solution.prototype.shuffle = function() {
+    for (let i = this.value.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [this.value[i], this.value[j]] = [this.value[j], this.value[i]];
+    }  
+
+  return this.value
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
